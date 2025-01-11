996073dcef8f6ad1297f13b1991fab555f5b7db4
K Vasantharan <vasantharank.learn@gmail.com>
2025-01-12 03:23:40 +0530

    Workflow completed v1

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "This is a home page"
 
+@app.route('/about')
+def about():
+    return "About page"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000,debug=True)
